1bc4cf7b41fc822baea8e02bd0e0818366088fbf
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-22 09:41:36 -0500

    taimen: always add manufacturer and model to recordings
    Change-Id: Ic9bb8ef119ac5016a2c782da8de1fd99518ce045
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- device-XOS.mk
+++ device-XOS.mk
@@ -1,2 +1,5 @@
 # halogenOS device makefile for wahoo
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-xos
+
+PRODUCT_PROPERTY_OVERRIDES += \
+      media.recorder.show_manufacturer_and_model=true
